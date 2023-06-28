5970d66c1488f9ce0fb94e9021073f50afeca129
Игорь Зацарин <dreadnought26@gmail.com>
2023-06-28 20:38:39 +0300

    Admin panel filters fix.

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -33,7 +33,7 @@ class UserViewSet(UserViewSet):
         user = request.user
         author = get_object_or_404(User, id=id)
         if request.method == 'POST':
-            Subscription.clean(user=user, author=author)
+            # Subscription.clean(user=user, author=author)
             serializer = UserSubSerializer(
                 data={'user': user.id, 'author': author.id},
                 context={'request': request},
